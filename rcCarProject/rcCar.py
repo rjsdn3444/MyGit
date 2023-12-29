bf4d83bbeb3c5a8909115b1de7020f4d4c3c7850
rjsdn3444 <rjsdn3444@gmail.com>
2023-12-29 17:32:50 +0900

    rcMotor.py의 Motor클래스가 LR모터 두개를 모두 가지고 있는 문제가 있어서 하나의 모터만 가지도록 수정, 이에 따라 rcCar.py도 수정

--- rcCarProject/rcCar.py
+++ rcCarProject/rcCar.py
@@ -1,25 +1,26 @@
 from rcMotor import rcMotor
 
 class rcCar:
-    def __init__(self):
-        self.__motor = rcMotor()
+    def __init__(self, forwardL=21, backwardL=20, forwardR=12, backwardR=16, minSpeed=0.18, maxSpeed=1.0):
+        self.__motorL = rcMotor(forwardL, backwardL, minSpeed, maxSpeed)
+        self.__motorR = rcMotor(forwardR, backwardR, minSpeed, maxSpeed)
 
     def forward(self, speed=0):
-        self.__motor.forwardMotorL(speed)
-        self.__motor.forwardMotorR(speed)
+        self.__motorL.forwardMotor(speed)
+        self.__motorR.forwardMotor(speed)
     
     def backward(self, speed=0):
-        self.__motor.backwardMotorL(speed)
-        self.__motor.backwardMotorR(speed)
+        self.__motorL.backwardMotor(speed)
+        self.__motorR.backwardMotor(speed)
 
     def rotateLeft(self, speed=0):
-        self.__motor.backwardMotorL(speed)
-        self.__motor.forwardMotorR(speed)
+        self.__motorL.backwardMotor(speed)
+        self.__motorR.forwardMotor(speed)
 
     def rotateRight(self, speed=0):
-        self.__motor.forwardMotorL(speed)
-        self.__motor.backwardMotorR(speed)
+        self.__motorL.forwardMotor(speed)
+        self.__motorR.backwardMotor(speed)
 
     def stop(self):
-        self.__motor.stopMotorL()
-        self.__motor.stopMotorR()
+        self.__motorL.stopMotor()
+        self.__motorR.stopMotor()
--- rcCarProject/rcMotor.py
+++ rcCarProject/rcMotor.py
@@ -1,42 +1,24 @@
 from gpiozero import Motor
 
 class rcMotor:
-    def __init__(self, forwardL=21, backwardL=20, forwardR=12, backwardR=16, minSpeed=0.18, maxSpeed=1.0):
-        self.__motorL = Motor(forward=forwardL, backward=backwardL)
-        self.__motorR = Motor(forward=forwardR, backward=backwardR)
+    def __init__(self, myForward, myBackward, minSpeed, maxSpeed):
+        self.__motor = Motor(forward=myForward, backward=myBackward)
         self.__minSpeed = minSpeed
         self.__maxSpeed = maxSpeed
     
-    def forwardMotorL(self, mySpeed=0):
+    def forwardMotor(self, mySpeed=0):
         if mySpeed < self.__minSpeed:
             mySpeed = self.__minSpeed
         if mySpeed > self.__maxSpeed:
             mySpeed = self.__maxSpeed
-        self.__motorL.forward(speed=mySpeed)
+        self.__motor.forward(speed=mySpeed)
 
-    def backwardMotorL(self, mySpeed=0):
+    def backwardMotor(self, mySpeed=0):
         if mySpeed < self.__minSpeed:
             mySpeed = self.__minSpeed
         if mySpeed > self.__maxSpeed:
             mySpeed = self.__maxSpeed
-        self.__motorL.backward(speed=mySpeed)
+        self.__motor.backward(speed=mySpeed)
 
-    def stopMotorL(self):
-        self.__motorL.stop()
-
-    def forwardMotorR(self, mySpeed=0):
-        if mySpeed < self.__minSpeed:
-            mySpeed = self.__minSpeed
-        if mySpeed > self.__maxSpeed:
-            mySpeed = self.__maxSpeed
-        self.__motorR.forward(speed=mySpeed)
-
-    def backwardMotorR(self, mySpeed=0):
-        if mySpeed < self.__minSpeed:
-            mySpeed = self.__minSpeed
-        if mySpeed > self.__maxSpeed:
-            mySpeed = self.__maxSpeed
-        self.__motorR.backward(speed=mySpeed)
-
-    def stopMotorR(self):
-        self.__motorR.stop()
+    def stopMotor(self):
+        self.__motor.stop()
